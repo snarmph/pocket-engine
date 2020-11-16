84c91c10087b48b72d3d7a54ad79062a0e0bf155
snarmph <ale55.bason@gmail.com>
2020-11-16 19:15:06 +0000

    First commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Pocket Engine
+Pokèmon clone engine
+To build:
+```bash
+cd build
+cmake -GNinja ../
+ninja
+``` 
---
+++ src/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+#include <SDL2/SDL.h>
+#include <GL/gl.h>
+#include <SDL2/SDL_opengl.h>
+
+int main() {
+    SDL_Init(SDL_INIT_EVERYTHING);
+
+    SDL_Window *window = SDL_CreateWindow(
+        "Pocket Engine",
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        500, 500,
+        SDL_WINDOW_OPENGL
+        );
+
+
+    while(true) {
+        SDL_Event e;
+
+        while(SDL_PollEvent(&e)) {
+            switch(e.type) {
+            case SDL_QUIT:
+                goto out_of_loop;
+                break;
+            }
+        }
+    }
+
+out_of_loop:
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+}
